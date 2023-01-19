2046e1d4b3deeb44c03435c9135132e052a06f00
BuildTools <unconfigured@null.spigotmc.org>
2023-01-19 11:09:53 -0500

    settup

---
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/ToggleableItemFrames.java
@@ -0,0 +1,39 @@
+package net.iamaprogrammer.toggleableitemframes;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ToggleableItemFrames.MODID)
+public class ToggleableItemFrames {
+
+    public static final String MODID = "toggleableitemframes";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ToggleableItemFrames()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
