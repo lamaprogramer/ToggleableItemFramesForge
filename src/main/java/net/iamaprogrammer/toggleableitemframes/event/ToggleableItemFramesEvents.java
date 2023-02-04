1c1b19a568e419735131454e6fb9e7ad499191c3
BuildTools <unconfigured@null.spigotmc.org>
2023-02-04 00:22:17 -0500

    updated to 2.0.0

--- src/main/java/net/iamaprogrammer/toggleableitemframes/event/ToggleableItemFramesEvents.java
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/event/ToggleableItemFramesEvents.java
@@ -3,7 +3,6 @@ package net.iamaprogrammer.toggleableitemframes.event;
 import net.iamaprogrammer.toggleableitemframes.ToggleableItemFrames;
 import net.iamaprogrammer.toggleableitemframes.isInvisible.EntityInvisible;
 import net.iamaprogrammer.toggleableitemframes.isInvisible.EntityInvisibleProvider;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,7 +10,9 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
@@ -29,47 +30,72 @@ public class ToggleableItemFramesEvents {
         static ServerLevel world;
         static ArrayList<ItemFrame> entities;
         static ArrayList<ItemFrame> invisibleFrames = new ArrayList<>();
-        
-        
+
+        static Level ClientWorld;
+        static ArrayList<ItemFrame> ClientEntities;
+
+        static ArrayList<ItemFrame> ClientInvisibleFrames = new ArrayList<>();
+
+
 
         @SubscribeEvent
         public static void onUseEntity(PlayerInteractEvent.EntityInteract event) {
-            if (event.getTarget() instanceof ItemFrame && event.getEntity() != null && !event.getSide().isClient()) {
-                if (event.getEntity().getMainHandItem().getItem() == Items.AIR && event.getEntity().isCrouching() && event.getHand() == InteractionHand.MAIN_HAND) {
-
+            if (event.getTarget() instanceof ItemFrame && !event.getPlayer().isSpectator() && !event.getSide().isClient() && event.getWorld().getServer() != null && event.getPlayer().getMainHandItem().isEmpty() && event.getPlayer().isCrouching() && event.getHand().equals(InteractionHand.MAIN_HAND)) {
+                if (!event.getWorld().getServer().isSingleplayer()) {
+                    
                     event.getTarget().setInvisible(!event.getTarget().isInvisible());
                     event.getTarget().getCapability(EntityInvisibleProvider.ENTITY_INVISIBLE).ifPresent(isCurrentlyInvisible -> {
                         isCurrentlyInvisible.setCurrentlyInvisible(event.getTarget().isInvisible());
-                        //event.getEntity().sendSystemMessage(Component.literal("was changed to" + event.getTarget().isInvisible()));
+                        //event.getPlayer().sendSystemMessage(Component.literal("was changed to" + event.getTarget().isInvisible()));
                     });
 
                     ((ItemFrame) event.getTarget()).setRotation(((ItemFrame) event.getTarget()).getRotation() - 1);
-                    event.getTarget().playSound(((ItemFrame) event.getTarget()).getRotateItemSound());
+                    if (((ItemFrame) event.getTarget()).getItem() == ItemStack.EMPTY) {
+                        event.getTarget().playSound(((ItemFrame) event.getTarget()).getRotateItemSound(), 1.0f, 1.0f);
+                    }
+                    //player.sendMessage(Text.literal("server"));
+
+                } else {
+                    event.getTarget().setInvisible(!event.getTarget().isInvisible());
+                    event.getTarget().getCapability(EntityInvisibleProvider.ENTITY_INVISIBLE).ifPresent(isCurrentlyInvisible -> {
+                        isCurrentlyInvisible.setCurrentlyInvisible(event.getTarget().isInvisible());
+                        //event.getPlayer().sendSystemMessage(Component.literal("was changed to" + event.getTarget().isInvisible()));
+                    });
+
+                    if (((ItemFrame) event.getTarget()).getItem() == ItemStack.EMPTY) {
+                        event.getTarget().playSound(((ItemFrame) event.getTarget()).getRotateItemSound(), 1.0f, 1.0f);
+                    }
+
+                    ((ItemFrame) event.getTarget()).setRotation(((ItemFrame) event.getTarget()).getRotation() - 1);
+                    //player.sendMessage(Text.literal("server"));
                 }
             }
         }
 
         @SubscribeEvent
-        public static void onServerTick(TickEvent.ServerTickEvent event) {
-            for (ServerPlayer player : event.getServer().getPlayerList().getPlayers()) {
-                world = player.getLevel();
-                entities = (ArrayList<ItemFrame>) world.getEntities(EntityTypeTest.forClass(ItemFrame.class), new AABB(player.getX() - 10, player.getY() - 10, player.getZ() - 10, player.getX() + 10, player.getY() + 10, player.getZ() + 10), EntitySelector.ENTITY_STILL_ALIVE);
-                for (ItemFrame frame : entities) {
-                    frame.getCapability(EntityInvisibleProvider.ENTITY_INVISIBLE).ifPresent(isCurrentlyInvisible -> {
-                        if (isCurrentlyInvisible.getCurrentlyInvisible()) {
-                            if ((player.isHolding(Items.ITEM_FRAME) || player.isHolding(Items.GLOW_ITEM_FRAME)) && Math.abs(player.getX() - frame.getX()) <= 9
-                                    && Math.abs(player.getY() - frame.getY()) <= 9
-                                    && Math.abs(player.getZ() - frame.getZ()) <= 9) {
-                                frame.setInvisible(false);
-                                invisibleFrames.add(frame);
-                            } else {
-                                frame.setInvisible(true);
+        public static void onServerTick(TickEvent.ServerTickEvent.WorldTickEvent event) {
+            
+            if (!event.world.getServer().isSingleplayer()) {
+                for (ServerPlayer player : event.world.getServer().getPlayerList().getPlayers()) {
+                    world = player.getLevel();
+                    entities = (ArrayList<ItemFrame>) world.getEntities(EntityTypeTest.forClass(ItemFrame.class), new AABB(player.getX() - 10, player.getY() - 10, player.getZ() - 10, player.getX() + 10, player.getY() + 10, player.getZ() + 10), EntitySelector.ENTITY_STILL_ALIVE);
+                    for (ItemFrame frame : entities) {
+                        frame.getCapability(EntityInvisibleProvider.ENTITY_INVISIBLE).ifPresent(isCurrentlyInvisible -> {
+                            if (isCurrentlyInvisible.getCurrentlyInvisible()) {
+                                if ((player.isHolding(Items.ITEM_FRAME) || player.isHolding(Items.GLOW_ITEM_FRAME)) && Math.abs(player.getX() - frame.getX()) <= 9
+                                        && Math.abs(player.getY() - frame.getY()) <= 9
+                                        && Math.abs(player.getZ() - frame.getZ()) <= 9) {
+                                    frame.setInvisible(false);
+                                    invisibleFrames.add(frame);
+                                } else {
+                                    frame.setInvisible(true);
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
+                    entities.clear();
+                    invisibleFrames.clear();
                 }
-                entities.clear();
-                invisibleFrames.clear();
             }
         }
 
---
+++ src/main/java/net/iamaprogrammer/toggleableitemframes/mixin/ModifyItemFrameRenderer.java
@@ -0,0 +1,33 @@
+package net.iamaprogrammer.toggleableitemframes.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemFrameRenderer;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemFrameRenderer.class)
+public abstract class ModifyItemFrameRenderer<T extends ItemFrame> extends EntityRenderer<T> {
+
+
+    protected ModifyItemFrameRenderer(EntityRendererProvider.Context p_174008_) {
+        super(p_174008_);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/world/entity/decoration/ItemFrame;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/decoration/ItemFrame;isInvisible()Z"))
+    private void injected(T p_115076_, float p_115077_, float p_115078_, PoseStack p_115079_, MultiBufferSource p_115080_, int p_115081_, CallbackInfo ci) {
+        if (!Minecraft.getInstance().isLocalServer() && Minecraft.getInstance().player != null) {
+            if (p_115076_.level.isClientSide() && !(Minecraft.getInstance().player.isHolding(Items.ITEM_FRAME) || Minecraft.getInstance().player.isHolding(Items.GLOW_ITEM_FRAME))) {
+                p_115076_.setInvisible(true);
+            }
+        }
+    }
+
+}
